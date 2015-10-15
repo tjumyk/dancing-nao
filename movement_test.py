80c0823c56e2d86a05b21d9fd452062be2f56c54
Lance Chen <lancelot_inbox@yahoo.com.au>
2015-10-15 11:48:13 +1100

    movement_test with custom pose library

---
+++ movement_test.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+import rospy
+from naoqi_bridge_msgs.msg import BodyPoseActionGoal
+
+def main():
+    move_publisher = rospy.Publisher("naoqi_msgs/BodyPose", BodyPoseActionGoal, queue_size=5)
+    move_publisher.publish("LeftArmUp")
+    rospy.spin()
+
+if __name__ == '__main__':
+    main()
+
+
