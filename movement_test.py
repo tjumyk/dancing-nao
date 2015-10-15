0dc32584fe6760dee24129d0e1c4c99c65003fde
Lance Chen <lancelot_inbox@yahoo.com.au>
2015-10-15 12:02:27 +1100

    movement_test node init

--- movement_test.py
+++ movement_test.py
@@ -4,6 +4,7 @@ import rospy
 from naoqi_bridge_msgs.msg import BodyPoseActionGoal
 
 def main():
+    rospy.init_node()
     move_publisher = rospy.Publisher("naoqi_msgs/BodyPose", BodyPoseActionGoal, queue_size=5)
     move_publisher.publish("LeftArmUp")
     rospy.spin()
