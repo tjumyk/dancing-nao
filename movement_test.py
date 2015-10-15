befd2737be5f39c7bb00295b9071b632d662a60d
Lance Chen <lancelot_inbox@yahoo.com.au>
2015-10-15 12:58:38 +1100

    movement_test test code

--- movement_test.py
+++ movement_test.py
@@ -1,13 +1,20 @@
 #!/usr/bin/env python
 
+#_xap:="/home/z5021315/catkin_ws/src/dancing-nao/poses.xap"
+
 import rospy
 from naoqi_bridge_msgs.msg import BodyPoseActionGoal
 
 def main():
-    rospy.init_node()
-    move_publisher = rospy.Publisher("naoqi_msgs/BodyPose", BodyPoseActionGoal, queue_size=5)
-    move_publisher.publish("LeftArmUp")
-    rospy.spin()
+    rospy.init_node("movement_test", anonymous=True)
+    move_publisher = rospy.Publisher("naoqi_bridge_msgs/BodyPoseGoal", BodyPoseActionGoal, queue_size=5)
+    rate = rospy.Rate(10)
+    while not rospy.is_shutdown():
+        move = BodyPoseActionGoal()
+        move.goal.pose_name = "leftArmUp"
+        move_publisher.publish(move)
+        rospy.loginfo("Move published")
+        rate.sleep()
 
 if __name__ == '__main__':
     main()
