58201b8d05e0a890a5a5f6e82ef1182c273ff24c
Lance Chen <lancelot_inbox@yahoo.com.au>
2015-10-20 10:37:17 +1100

    movement_test working

--- movement_test.py
+++ movement_test.py
@@ -7,14 +7,24 @@ from naoqi_bridge_msgs.msg import BodyPoseActionGoal
 
 def main():
     rospy.init_node("movement_test", anonymous=True)
-    move_publisher = rospy.Publisher("naoqi_bridge_msgs/BodyPoseGoal", BodyPoseActionGoal, queue_size=5)
+    move_publisher = rospy.Publisher("/body_pose/goal", BodyPoseActionGoal, queue_size=1)
     rate = rospy.Rate(10)
     while not rospy.is_shutdown():
         move = BodyPoseActionGoal()
-        move.goal.pose_name = "leftArmUp"
+        move.goal.pose_name = "crouch"
         move_publisher.publish(move)
         rospy.loginfo("Move published")
         rate.sleep()
+    #rospy.wait_for_service("body_pose")
+    #try:
+    #    execute_pose = rospy.ServiceProxy("body_pose", BodyPoseActionGoal)
+    #    move = BodyPoseActionGoal()
+    #    move.goal.pose_name = "leftArmUp"
+    #    execute_pose(move)
+    #    rospy.loginfo("Service call made")
+    #except rospy.ServiceException, e:
+    #    print ("Service call failed: %s"%e)
+
 
 if __name__ == '__main__':
     main()
